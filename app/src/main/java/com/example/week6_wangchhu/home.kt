e115e1d09ba666571f33d3b21dd3f3a0f7e397d7
wangc <wangchhutamang123@gmail.com>
2021-01-15 13:35:30 +0545

    Initial commit

--- app/src/main/java/com/example/week6_wangchhu/home.kt
+++ app/src/main/java/com/example/week6_wangchhu/home.kt
@@ -2,10 +2,50 @@ package com.example.week6_wangchhu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import fragments.aBOUT
+import fragments.aDD
+import fragments.hOME
 
 class home : AppCompatActivity() {
+    private lateinit var nav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        nav =findViewById(R.id.btmNav)
+
+        if (savedInstanceState== null)
+        {
+            val fragment = hOME()
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                .commit()
+        }
+        nav.setOnNavigationItemSelectedListener(selectedMenuItem)
     }
-}
+
+    private val selectedMenuItem = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+        when (menuItem.itemId){
+            R.id.home ->{
+                val fragment =hOME()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                    .commit()
+                return@OnNavigationItemSelectedListener true
+
+            } R.id.addStd ->{
+            val fragment = aDD()
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                .commit()
+            return@OnNavigationItemSelectedListener true
+
+        } R.id.about ->{
+            val fragment = aBOUT()
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                .commit()
+            return@OnNavigationItemSelectedListener true
+
+        }
+        }
+        false
+    }
--- app/src/main/java/fragments/aBOUT.kt
+++ app/src/main/java/fragments/aBOUT.kt
@@ -1,4 +1,40 @@
 package fragments
 
-class aBOUT {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.week6_wangchhu.R
+
+class aBOUT : Fragment() {
+    private lateinit var wbView: WebView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =inflater.inflate(R.layout.fragment_a_b_o_u_t, container, false)
+        wbView= view.findViewById(R.id.wbView)
+        wbView.loadUrl("https://softwarica.edu.np/")
+
+        val WebSettings = wbView.getSettings();
+        WebSettings.setJavaScriptEnabled(true)
+
+        wbView.setWebViewClient(WebViewClient())
+
+
+
+        return view
+
+    }
+
+
+
+
+
+
 }
